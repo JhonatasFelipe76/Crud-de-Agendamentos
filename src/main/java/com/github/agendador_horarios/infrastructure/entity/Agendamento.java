b139f571955607584c797fe38e6b4198fe1dec98
JhonatasFelipe76 <jhonatasestudos571@gmail.com>
2026-07-01 09:15:55 +0100

    Entidade Agendamento

--- src/main/java/com/github/agendador_horarios/infrastructure/entity/Agendamento.java
+++ src/main/java/com/github/agendador_horarios/infrastructure/entity/Agendamento.java
@@ -1,4 +1,31 @@
 package com.github.agendador_horarios.infrastructure.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "agendamento")
 public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String servico;
+    private String professional;
+    private LocalDateTime dataHoraAgendamento;
+    private String cliente;
+    private String telefoneCliente;
+    private LocalDateTime dataInsercao = LocalDateTime.now();
+
+
+
 }
